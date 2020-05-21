c1c8119f138733c1a2b996b59f14c7161cab250a
Zhidong Zhu <zhuzhidong@gmail.com>
2020-05-21 18:57:11 +0800

    add license header

--- specs/samples/modules/mpi-modules/mpi-train-mnist/mnist/pytorch_horovod_mnist.py
+++ specs/samples/modules/mpi-modules/mpi-train-mnist/mnist/pytorch_horovod_mnist.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 # Copyright (c) 2017, PyTorch contributors
 # Modifications copyright (C) Microsoft Corporation
 # Licensed under the BSD license
--- specs/samples/modules/mpi-modules/mpi-train-mnist/mnist/sample_image_generator.py
+++ specs/samples/modules/mpi-modules/mpi-train-mnist/mnist/sample_image_generator.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 from torchvision.datasets import MNIST
 from pathlib import Path
 import argparse
